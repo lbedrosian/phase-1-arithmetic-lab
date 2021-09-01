ccfbb0d7b87d63fbe5a14e132bfabbc3afa95962
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-09-01 06:55:12 -0400

    commit tree 80ad9fe5cfa942adc83650dbd7f823051966dfe7

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let multiply
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2
 
+let random
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod
+let num3 = 4
+let num4 = 5
+mod = num3 % num4
+
+let max
+max = Math.max(5, 10, 15, 20)
 // Write your code here
